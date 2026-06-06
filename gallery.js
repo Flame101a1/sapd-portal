1d19902571674dda641c800e689fff1aaf5e8a1a
FlameOptics <noahthornton53@gmail.com>
2026-06-06 16:41:48 -0400

    Update website files

--- gallery.js
+++ gallery.js
@@ -1,10 +1,82 @@
 const galleryImages = [
 
 {
+image:"images/gallery/hero1.png",
+title:"Patrol Photo",
+description:"SAPD High Command."
+},
+
+{
+image:"images/gallery/hero2.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
 image:"images/gallery/hero3.png",
 title:"Patrol Photo",
 description:"SAPD patrol operations."
-}
+},
+
+{
+image:"images/gallery/hero4.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
+image:"images/gallery/hero5.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
+image:"images/gallery/hero6.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
+image:"images/gallery/hero7.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
+image:"images/gallery/hero8.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
+image:"images/gallery/hero9.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
+image:"images/gallery/hero10.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
+image:"images/gallery/hero11.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
+image:"images/gallery/hero12.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
+
+{
+image:"images/gallery/hero13.png",
+title:"Patrol Photo",
+description:"SAPD patrol operations."
+},
 
 ];
 
